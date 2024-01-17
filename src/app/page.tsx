1ad8ac1b7dce39fbe01a9e86fdf6649fb206b6e3
Mo <m.adel.abdelhady@gmail.com>
2024-01-17 20:42:50 +0100

    Added publications and projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -183,6 +183,48 @@ export default function Page() {
             })}
           </div>
         </Section>
+
+        <Section>
+          <h2 className="text-xl font-bold">Publications</h2>
+          {RESUME_DATA.publications.map((pub) => {
+            return (
+              <Card key={pub.title}>
+                <CardHeader>
+                  <div className="flex items-center justify-between gap-x-2 text-base">
+                    <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
+                      <a className="hover:underline" href={pub.link}>
+                        {pub.title}
+                      </a>
+
+                      <span className="inline-flex gap-x-1">
+                        {pub.badges.map((badge) => (
+                          <Badge
+                            variant="secondary"
+                            className="align-middle text-xs"
+                            key={badge}
+                          >
+                            {badge}
+                          </Badge>
+                        ))}
+                      </span>
+                    </h3>
+                    <div className="text-sm tabular-nums text-gray-500">
+                      {pub.year}
+                    </div>
+                  </div>
+
+                  <h4 className="font-mono text-sm leading-none">
+                    {pub.conference}
+                  </h4>
+                </CardHeader>
+                <CardContent className="mt-2 text-xs mb-4">
+                  Authors: {pub.authors}
+                </CardContent>
+              </Card>
+            );
+          })}
+        </Section>
+
       </section>
 
       <CommandMenu
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -27,7 +27,7 @@ export const RESUME_DATA = {
   about:
     "Machine Learning Tech Lead, focused on building products with extra attention to detail",
   summary:
-    "As a Machine Learning Tech Lead, I have successfully taken multiple products and teams from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with Python to build pipelines, models, and tools to analyze billions of transactions for anti-money laundering and fraud detection applications. I have over 8 years of experience in working with companies all around the world.",
+    "As a Machine Learning Tech Lead, I have successfully taken multiple products and teams from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with Python to data-driven products to analyze billions of transactions daily in order to prevent money laundering and fraud. I have over 8 years of experience in working with companies all around the world on different scales.",
   avatarUrl: "/profile-img.jpeg",
   personalWebsiteUrl: "https://moabdelhady.com",
   contact: {
@@ -60,7 +60,7 @@ export const RESUME_DATA = {
     },
     {
       school: "German University in Cairo",
-      degree: "BSc in Mechatronics - Full Scholarship",
+      degree: "BSc in Mechatronics - 60% Scholarship",
       start: "2009",
       end: "2014",
     },
@@ -109,16 +109,57 @@ export const RESUME_DATA = {
       end: "2018",
       description: "Lead the technical onboarding and deployment of autonomous fleets at 2 intra-logistics warehouses. Develop navigation algorithms based on topological graphs for multi-robot applications.",
     },
+    {
+      company: "German University in Cairo",
+      link: "https://www.guc.edu.eg/",
+      badges: [],
+      title: "Research & Teaching Assistant",
+      logo: NSNLogo,
+      start: "2014",
+      end: "2015",
+      description: "Teaching of undergraduate course and developing multi-robot planning and task allocation algorithms for heterogeneous Khepera-III mobile robots",
+    },
   ],
   skills: [
     "Python",
     "Pyspark",
     "Keras/TensorFlow",
     "Pytorch",
+    "FastAPI",
     "JavaScript",
     "React/Next.js",
   ],
   projects: [
+    {
+      title: "Marketplace Fraud detection",
+      techStack: [
+        "Python",
+        "Pyspark",
+        "Pytorch",
+        "GNN"
+      ],
+      description: "Developing graph-based machine learning models to detect and prevent seller fraud on marketplaces.",
+      logo: ConsultlyLogo,
+      link: {
+        label: "Article",
+        href: "https://www.adyen.com/knowledge-hub/combating-marketplace-seller-fraud-with-graph-neural-networks",
+      },
+    },
+    {
+      title: "Fine-grained Image Classification",
+      techStack: [
+        "TensorFlow",
+        "FastAPI",
+        "AWS",
+        "Computer Vision"
+      ],
+      description: "Research and development of identity document authentication models for KYC applications.",
+      logo: ConsultlyLogo,
+      link: {
+        label: "Article",
+        href: "",
+      },
+    },
     {
       title: "Tensorflow Metrics",
       techStack: [
@@ -145,137 +186,82 @@ export const RESUME_DATA = {
         href: "https://imfdatainsights.com/",
       },
     },
-    // {
-    //   title: "Jarocki.me",
-    //   techStack: ["Side Project", "Next.js", "MDX"],
-    //   description:
-    //     "My personal website and blog. Built with Next.js and Notion API",
-    //   logo: JarockiMeLogo,
-    //   link: {
-    //     label: "github.com",
-    //     href: "https://jarocki.me/",
-    //   },
-    // },
-    // {
-    //   title: "Minimal",
-    //   techStack: ["Side Project", "Next.js", "Puppeteer"],
-    //   description:
-    //     "Minimalist calendars, habit trackers and planners generator",
-    //   logo: Minimal,
-    //   link: {
-    //     label: "useminimal.com",
-    //     href: "https://useminimal.com/",
-    //   },
-    // },
-    // {
-    //   title: "Barepapers",
-    //   techStack: ["Side Project", "Next.js", "Puppeteer"],
-    //   description:
-    //     "Generates beautiful wallpapers using random shapes and gradients",
-    //   logo: BarepapersLogo,
-    //   link: {
-    //     label: "barepapers.com",
-    //     href: "https://barepapers.com/",
-    //   },
-    // },
-    // {
-    //   title: "Year progress",
-    //   techStack: ["Side Project", "TypeScript", "Next.js"],
-    //   description: "Tracks current year progress and displays a countdown",
-    //   logo: YearProgressLogo,
-    //   link: {
-    //     label: "getyearprogress.com",
-    //     href: "https://getyearprogress.com/",
-    //   },
-    // },
-    // {
-    //   title: "Parabol",
-    //   techStack: [
-    //     "Full Stack Developer",
-    //     "TypeScript",
-    //     "React",
-    //     "Node.js",
-    //     "GraphQL",
-    //   ],
-    //   description:
-    //     "The Agile meeting co-pilot that delivers better meetings with less effort",
-    //   logo: ParabolLogo,
-    //   link: {
-    //     label: "github.com",
-    //     href: "https://parabol.co/",
-    //   },
-    // },
-    // {
-    //   title: "Evercast",
-    //   techStack: [
-    //     "Lead Frontend Developer",
 
-    //     "TypeScript",
-    //     "React",
-    //     "Node.js",
-    //     "GraphQL",
-    //   ],
-    //   description:
-    //     "Creative collaboration platform that combines video conferencing and HD media streaming",
-    //   logo: EvercastLogo,
-    //   link: {
-    //     label: "evercast.us",
-    //     href: "https://www.evercast.us/",
-    //   },
-    // },
-    // {
-    //   title: "Mobile Vikings",
-    //   techStack: ["Lead Android Developer", "Android", "Kotlin"],
-    //   description:
-    //     "Android application for leading virtual mobile operator in Poland",
-    //   logo: MobileVikingsLogo,
-    //   link: {
-    //     label: "mobilevikings.pl",
-    //     href: "https://mobilevikings.pl/",
-    //   },
-    // },
-    // {
-    //   title: "Howdy",
-    //   techStack: ["Lead Android Developer", "Android", "Kotlin"],
-    //   description:
-    //     "Howdy is a place for you to join communities you care about",
-    //   logo: Howdy,
-    //   link: {
-    //     label: "play.google.com",
-    //     href: "https://howdy.co/",
-    //   },
-    // },
-    // {
-    //   title: "Tastycloud",
-    //   techStack: ["Lead Android Developer", "Android", "Kotlin"],
-    //   description:
-    //     "Android application for managing and displaying restaurant menus in kiosk mode",
-    //   logo: TastyCloudLogo,
-    //   link: {
-    //     label: "tastycloud.fr",
-    //     href: "https://www.tastycloud.fr/",
-    //   },
-    // },
-    // {
-    //   title: "Ambit",
-    //   techStack: ["Lead Android Developer", "Android", "Kotlin"],
-    //   description:
-    //     "Android application that helps with sharing your contact details",
-    //   logo: AmbitLogo,
-    // },
-    // {
-    //   title: "Bim",
-    //   techStack: ["Lead Android Developer", "Android", "Kotlin"],
-    //   description:
-    //     "Android application that helps with booking a table in a restaurants",
-    //   logo: BimLogo,
-    // },
-    // {
-    //   title: "Canal Digital GO",
-    //   techStack: ["Lead Android Developer", "Android", "Kotlin"],
-    //   description:
-    //     "Video streaming mobile application for Canal Digital subscribers",
-    //   logo: CDGOLogo,
-    // },
+    {
+      title: "RL Traffic Optimizer",
+      techStack: ["Hackathon", "Python", "Pytorch", "RL"],
+      description:
+        "Reinforcement learning approach for traffic lights optimization (2nd place in C4F2 Hackathon)",
+      logo: MonitoLogo,
+      link: {
+        label: "Leaderboard",
+        href: "https://c4f2.ai.gov.eg/hackathon-closing/",
+      },
+    },
+
+    {
+      title: "Kaggle Terrain Classification Competition",
+      techStack: ["Kaggle", "Python", "Keras", "Time-series"],
+      description:
+        "Utilizing robots IMU data for terrain classification task. Mixing a frequency-domain representation approach with LSTM to achieve top 6%",
+      logo: MonitoLogo,
+      link: {
+        label: "Leaderboard",
+        href: "https://github.com/adelizer/kaggle-sandbox",
+      },
+    },
+
+    {
+      title: "Python Robotics",
+      techStack: ["Robotics", "Python", "SLAM", "Optimization"],
+      description:
+        "Contributing to one of the most popular robotics libraries in Python, implementing SLAM algorithms with detailed explanation and notebooks.",
+      logo: MonitoLogo,
+      link: {
+        label: "Contributions",
+        href: "https://github.com/AtsushiSakai/PythonRobotics",
+      },
+    },
+
+
   ],
+  publications: [
+    {
+      title: "Reuse-oriented SLAM Framework using Software Product Lines",
+      link: "https://ieeexplore.ieee.org/document/9287895",
+      badges: [],
+      conference: "IEEE International Conference on Robotic Computing (IRC)",
+      year: "2020",
+      authors:
+        "Mohamed A. Abdelhady, Douwe D. Dresscher, Jan F. Broenink",
+    },
+    {
+      title: "Learning A Recurrent Neural Network for State Estimation using Filter Data",
+      link: "https://ieeexplore.ieee.org/document/8814019",
+      badges: [],
+      conference: "Intelligent Vehicles Symposium (IV)",
+      year: "2019",
+      authors:
+        "Ahmed M. Hammam, Mohamed A. Abdelhady, Omar Shehata",
+    },
+
+    {
+      title: "Demo Design: The VIRO Agile Eye",
+      link: "http://www.dspe.nl/cms_file.php?fromDB=2133",
+      badges: [],
+      conference: "DSPE Mikroniek magazine, Volume 58, Issue 1",
+      year: "2018",
+      authors:
+        "Louis Stijn, Mohamed A. Abdelhady, Samer Abdelmoeti",
+    },
+    {
+      title: "Multi-robot Task Allocation for Search and Rescue Missions",
+      link: "http://iopscience.iop.org/article/10.1088/1742-6596/570/5/052006/pdf",
+      badges: [],
+      conference: "European Workshop on Advanced Control and Diagnosis",
+      year: "2014",
+      authors:
+        "A. Hussein, Mohamed A. Abdelhady, M. Bakr, Omar Shehata, Alaa Khamis",
+    },
+]
 } as const;
